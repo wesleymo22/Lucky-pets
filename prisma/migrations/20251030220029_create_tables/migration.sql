f58cca8571be2aeb8ddb1dc8be6ece205f644705
Wesley Moraes Franco <wesleymo22@gmail.com>
2025-10-30 19:04:29 -0300

    feat: iniciando crud usuario e auth

--- prisma/migrations/20251030220029_create_tables/migration.sql
+++ prisma/migrations/20251030220029_create_tables/migration.sql
@@ -1,35 +1,35 @@
 -- CreateTable
-CREATE TABLE `Usuario` (
+CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(191) NOT NULL,
-    `senha` VARCHAR(191) NOT NULL,
-    `nome` VARCHAR(191) NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    UNIQUE INDEX `Usuario_email_key`(`email`),
+    UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Pet` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `nome` VARCHAR(191) NOT NULL,
-    `especie` VARCHAR(191) NOT NULL,
-    `idade` INTEGER NULL,
-    `peso` DOUBLE NULL,
-    `observacoes` VARCHAR(191) NULL,
-    `donoId` INTEGER NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `species` VARCHAR(191) NOT NULL,
+    `age` INTEGER NULL,
+    `weight` DOUBLE NULL,
+    `notes` VARCHAR(191) NULL,
+    `ownerId` INTEGER NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Agendamento` (
+CREATE TABLE `Appointment` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `data` DATETIME(3) NOT NULL,
-    `servico` VARCHAR(191) NOT NULL,
-    `observacoes` VARCHAR(191) NULL,
+    `date` DATETIME(3) NOT NULL,
+    `service` VARCHAR(191) NOT NULL,
+    `notes` VARCHAR(191) NULL,
     `status` VARCHAR(191) NOT NULL DEFAULT 'scheduled',
     `petId` INTEGER NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
@@ -38,7 +38,7 @@ CREATE TABLE `Agendamento` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Pet` ADD CONSTRAINT `Pet_donoId_fkey` FOREIGN KEY (`donoId`) REFERENCES `Usuario`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Pet` ADD CONSTRAINT `Pet_ownerId_fkey` FOREIGN KEY (`ownerId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Agendamento` ADD CONSTRAINT `Agendamento_petId_fkey` FOREIGN KEY (`petId`) REFERENCES `Pet`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Appointment` ADD CONSTRAINT `Appointment_petId_fkey` FOREIGN KEY (`petId`) REFERENCES `Pet`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
