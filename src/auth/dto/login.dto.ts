f6a5f345d12b7f699fc587f416d63c1a4a9b7aef
Wesley Moraes Franco <wesleymo22@gmail.com>
2025-10-31 21:28:07 -0300

    fix: login fixes

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -3,11 +3,10 @@ import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
   @ApiProperty({ example: 'usuario@teste.com', description: 'E-mail do usuário' })
-  @IsEmail()
+  @IsString()
   email: string;
 
   @ApiProperty({ example: '123456', description: 'Senha do usuário (mínimo 6 caracteres)' })
   @IsString()
-  @MinLength(6)
   password: string;
 }
